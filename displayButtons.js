05e46cf7197e63353954b0354f691d11268e4f5d
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-07 16:04:45 +0430

    init

--- displayButtons.js
+++ displayButtons.js
@@ -1,6 +1,6 @@
 const displayButtons = (container, pages, activeIndex) => {
     let btns = pages.map((_, pageIndex) => {
-        return `<button class="page-btn">${pageIndex + 1}</button>`
+        return `<button class="page-btn ${activeIndex === pageIndex ? 'active-btn': 'null'}">${pageIndex + 1}</button>`
     })
     container.innerHTML = btns.join('')
 }
