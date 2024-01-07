963836397b0defee5794fda5999225cd071b2545
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-07 14:26:06 +0430

    init

--- paginate.js
+++ paginate.js
@@ -2,7 +2,7 @@ const paginate = (followers) => {
     const itemPerPage = 10
     const numberOfPages = Math.ceil(followers.length / itemPerPage) 
     const newFollowers = Array.from({length:numberOfPages}, (_, index) => {
-        
+        const start = index * itemPerPage
     })
 }
 
