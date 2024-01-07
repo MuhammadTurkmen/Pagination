78d64d87cb3a3423f6b15c79f16628aaf03eefc7
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-07 19:08:08 +0430

    init

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const init = async () => {
 }
  
 btnContainer.addEventListener('click', function(e) {
-    if(e.target.classList.contains('btn-container')) 
+    if(e.target.classList.contains('btn-container')) return
     if(e.target.classList.contains('page-btn')) {
        index = parseInt(e.target.dataset.index)
     }
