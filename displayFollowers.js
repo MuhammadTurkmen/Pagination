80238654bd24595ce75ec4d8b88983bbde0c5e91
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-07 14:01:08 +0430

    init

--- displayFollowers.js
+++ displayFollowers.js
@@ -7,7 +7,7 @@ const display = (followers) => {
         return `<article class="card">
                     <img src="${avatar_url}" alt="${login}"/>
                     <h4>${login}</h4>
-                    <a href="${html_url}" class="btn"></a>
+                    <a href="${html_url}" class="btn">view profile</a>
                 </article>`
     })
 }
