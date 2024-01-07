6a66d8694bbe0b086f17c65445badeb59b0ed089
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-07 14:33:54 +0430

    init

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const title = document.querySelector('.section-title h1')
 
 const init = async () => {
     const followers = await fetchFollowers()
-    displayFollowers(paginate(followers)[])
+    displayFollowers(paginate(followers)[0])
     title.textContent = 'pagination'
     const pages = paginate(followers)
 }
