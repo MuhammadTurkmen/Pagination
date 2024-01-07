eaf84b2b641b2de998e561927e0bbb0931c479b3
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-07 13:55:24 +0430

    init

--- displayFollowers.js
+++ displayFollowers.js
@@ -4,7 +4,9 @@ const container = document.querySelector('.container')
 const display = (followers) => {
     const newFollowers = followers.map((person) => {
         const {avatar_url, login, html_url} = person
-        return `<article class="card"></article>`
+        return `<article class="card">
+                    <img src="${avatar_url}"/>
+                </article>`
     })
 }
 
