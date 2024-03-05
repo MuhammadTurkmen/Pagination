d0f04de45b22f4a2e2da9449705fa95374ba72d0
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-05 13:45:30 +0430

    init

--- displayFollowers.js
+++ displayFollowers.js
@@ -3,7 +3,7 @@ const container = document.querySelector('.container')
 
 const display = (followers) => {
     const newFollowers = followers.map((person) => {
-
+        const {} = person
     })
 }
 
