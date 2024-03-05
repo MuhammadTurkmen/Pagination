2348b2716ab987e26398accda26a571736658ed8
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-05 12:28:37 +0430

    init

--- fetchFollowers.js
+++ fetchFollowers.js
@@ -2,6 +2,7 @@ const url = 'https://api.github.com/users/john-smilga/followers?per_page=100'
 
 const fetchFollowers = async () => {
     const response = await fetch(url)
+    const data  = await response.json()
 }
 
 export default fetchFollowers
