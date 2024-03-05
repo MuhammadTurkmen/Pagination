cccb805823973c91aa8281b88d9c10787390cbba
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-05 12:34:11 +0430

    init

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ import fetchFollowers from './fetchFollowers.js'
 import displayFollowers from './displayFollowers.js'
 import paginate from './paginate.js'
 import displayButtons from './displayButtons.js'
+
+window.addEventListener('load', init)
